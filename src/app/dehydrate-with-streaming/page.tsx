33c89c597d96ffaf7f004e687c49d2ddd412dead
Eunkyung-Son <sek9510@naver.com>
2024-04-25 12:06:47 +0900

    add skeleton

--- src/app/dehydrate-with-streaming/page.tsx
+++ src/app/dehydrate-with-streaming/page.tsx
@@ -1,19 +1,21 @@
 import HydratedPhotoList from "@/components/Photo/hydratedPhotoList";
+import { RenderingPageSkeleton } from "@/components/RenderingPageSkeleton";
+import { SkeletonCard } from "@/components/SkeletonCard";
 import HydratedTodoList from "@/components/Todo/hydratedTodoList";
 import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function DehydrateWithStreaming() {
   return (
-    <main className="flex min-h-screen flex-row p-24">
+    <main className="flex min-h-screen h-full flex-row p-24">
       <h1>tanstack-query dehydrate & fetch & streaming</h1>
       <Link href="/">go to home</Link>
       <section className="flex min-h-screen flex-row p-24">
-        <Suspense fallback={"todo loading!!!"}>
+        <Suspense fallback={<SkeletonCard />}>
           {/* @ts-expect-error Async Server Component */}
           <HydratedTodoList />
         </Suspense>
-        <Suspense fallback={"photo loading!!!"}>
+        <Suspense fallback={<RenderingPageSkeleton />}>
           {/* @ts-expect-error Async Server Component */}
           <HydratedPhotoList />
         </Suspense>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24">
+    <main className="flex min-h-screen flex-col gap-8 p-24">
       <h1>Various examples</h1>
-      <Link href="/dehydrate-with-streaming">
+      <Link
+        className="w-auto h-fit border-white-100 rounded-12 p-12 text-black hover:bg-blue-100"
+        href="/dehydrate-with-streaming"
+      >
         go to dehydrate-with-steaming
       </Link>
       <Link href="/fetch-with-streaming">go to fetch-with-streaming</Link>
