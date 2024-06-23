c442427cf790f61ca845623dc6e285fcf9002821
EunkyungSon <sek9510@naver.com>
2024-06-23 22:36:23 +0900

    add example page list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,26 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-8 p-24">
-      <h1>Various examples</h1>
+    <main className="flex min-h-screen flex-col gap-8 p-24 bg-gray-50">
+      <h1 className="text-3xl font-bold mb-8 text-black">Various Examples</h1>
       <Link
-        className="w-auto h-fit border-white-100 rounded-12 p-12 text-black hover:bg-blue-100"
+        className="w-auto h-fit border border-gray-300 rounded-lg p-4 text-black hover:bg-blue-100"
         href="/dehydrate-with-streaming"
       >
-        go to dehydrate-with-steaming
+        Go to Dehydrate with Streaming
       </Link>
-      <Link href="/fetch-with-streaming">go to fetch-with-streaming</Link>
-      <Link href="/client-tanstack-query">go to client-tanstack-query</Link>
-      <Link href="/client-tanstack-query-2">go to client-tanstack-query-2</Link>
+      <Link
+        className="w-auto h-fit border border-gray-300 rounded-lg p-4 text-black hover:bg-blue-100"
+        href="/fetch-with-streaming"
+      >
+        Go to Fetch with Streaming
+      </Link>
+      <Link
+        className="w-auto h-fit border border-gray-300 rounded-lg p-4 text-black hover:bg-blue-100"
+        href="/client-tanstack-query"
+      >
+        Go to Client Tanstack Query
+      </Link>
     </main>
   );
 }
